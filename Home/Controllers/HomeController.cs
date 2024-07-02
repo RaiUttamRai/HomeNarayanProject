6c002afca4a40f6d906cc6efa15640a256e4f94c
Uttam Rai <raiu2087@gmail.com>
2024-07-02 23:38:24 +0545

    Add project files.

---
+++ Home/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using Home.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using System.Diagnostics;
+
+namespace Home.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Services()
+        {
+            return View();
+        }
+
+        public IActionResult Work()
+        {
+            return View();
+        }
+        public IActionResult Contact() 
+        { 
+            return View(); 
+        }
+    }
+}
